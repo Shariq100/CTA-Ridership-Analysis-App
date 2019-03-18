0d10b9c9472ac3eaa83d7072b90c710bbb1a72b1
Shariq100 <45380083+Shariq100@users.noreply.github.com>
2019-03-18 15:22:17 -0500

    Added Files
    Created a repository of CTA-Ridership-Analysis-App

---
+++ CTA/BusinessTierLogic.cs
@@ -0,0 +1,436 @@
+﻿//
+// BusinessTier:  business logic, acting as interface between UI and data store.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace BusinessTier
+{
+
+  //
+  // Business:
+  //
+  public class Business
+  {
+    //
+    // Fields:
+    //
+    private string _DBFile;
+    private DataAccessTier.Data dataTier;
+
+
+    ///
+    /// <summary>
+    /// Constructs a new instance of the business tier.  The format
+    /// of the filename should be either |DataDirectory|\filename.mdf,
+    /// or a complete Windows pathname.
+    /// </summary>
+    /// <param name="DatabaseFilename">Name of database file</param>
+    /// 
+    public Business(string DatabaseFilename)
+    {
+      _DBFile = DatabaseFilename;
+
+      dataTier = new DataAccessTier.Data(DatabaseFilename);
+    }
+
+
+    ///
+    /// <summary>
+    ///  Opens and closes a connection to the database, e.g. to
+    ///  startup the server and make sure all is well.
+    /// </summary>
+    /// <returns>true if successful, false if not</returns>
+    /// 
+    public bool TestConnection()
+    {
+      return dataTier.OpenCloseConnection();
+    }
+
+
+    ///
+    /// <summary>
+    /// Returns all the CTA Stations, ordered by name.
+    /// </summary>
+    /// <returns>Read-only list of CTAStation objects</returns>
+    /// 
+    public IReadOnlyList<CTAStation> GetStations()
+    {
+      List<CTAStation> stations = new List<CTAStation>();
+
+      try
+      {
+
+                //
+                // TODO!
+                //
+                DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+                string sql = "Select * from Stations Order By Stations.Name";
+                DataSet ds = dataTier.ExecuteNonScalarQuery(sql);
+
+                foreach (DataRow row in ds.Tables["TABLE"].Rows)
+                {
+                    int stationID = Convert.ToInt32(row["StationID"]);  // all rows have same station ID:
+                    string stationName = Convert.ToString(row["Name"]);
+                    stations.Add(new CTAStation(stationID, stationName));
+                }
+
+
+                return stations;
+                //object result = dataTier.ExecuteScalarQuery(sql);
+
+            }
+      catch (Exception ex)
+      {
+        string msg = string.Format("Error in Business.GetStations: '{0}'", ex.Message);
+        throw new ApplicationException(msg);
+      }
+
+      return stations;
+    }
+
+    //********************//**********************//
+        
+    //*******************//**********************//
+    ///
+    /// <summary>
+    /// Returns the CTA Stops associated with a given station,
+    /// ordered by name.
+    /// </summary>
+    /// <returns>Read-only list of CTAStop objects</returns>
+    ///
+    public IReadOnlyList<CTAStop> GetStops(int stationID)
+    {
+      List<CTAStop> stops = new List<CTAStop>();
+      try
+      {
+                //
+                // TODO!
+                //
+                DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+                string sql = string.Format(@"
+                SELECT * From Stops
+                INNER JOIN Stations ON Stops.StationID = Stations.StationID
+                WHERE Stations.StationID = '{0}' 
+                ORDER BY Stops.Name ASC;
+                ", stationID);
+
+                DataSet ds = dataTier.ExecuteNonScalarQuery(sql);
+
+                foreach (DataRow row in ds.Tables["TABLE"].Rows)
+                {
+                    int stopID = Convert.ToInt32(row["StopID"]);
+                    string stopName = Convert.ToString(row["Name"]);
+                    string direction = Convert.ToString(row["Direction"]);
+                    bool ada = Convert.ToBoolean(row["ADA"]);
+                    double latitude = Convert.ToDouble(row["Latitude"]);
+                    double longitude = Convert.ToDouble(row["Longitude"]);
+                    stops.Add(new CTAStop(stopID, stopName, stationID, direction, ada, latitude, longitude));
+                }
+
+                return stops;
+            }
+      catch (Exception ex)
+      {
+                //foreach(var s in stops)
+                //{
+                //    Console.Out.WriteLine(s);
+                //}
+                Console.Out.WriteLine("test 4");
+        string msg = string.Format("Error in Business.GetStops: '{0}'", ex.Message);
+        throw new ApplicationException(msg);
+      }
+
+      return stops;
+    }
+
+
+    ///
+    /// <summary>
+    /// Returns the top N CTA Stations by ridership, 
+    /// ordered by name.
+    /// </summary>
+    /// <returns>Read-only list of CTAStation objects</returns>
+    /// 
+    public IReadOnlyList<CTAStation> GetTopStations(int N)
+    {
+      if (N < 1)
+        throw new ArgumentException("GetTopStations: N must be positive");
+
+      List<CTAStation> stations = new List<CTAStation>();
+
+      try
+      {
+
+                //
+                // TODO!
+                //
+                DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+                string sql = string.Format(@"
+                SELECT Top 10 Name, Sum(DailyTotal) As TotalRiders
+                FROM Riderships
+                INNER JOIN Stations ON Riderships.StationID = Stations.StationID 
+                GROUP BY Stations.StationID, Name
+                ORDER BY TotalRiders DESC;
+                ");
+
+                DataSet ds = dataTier.ExecuteNonScalarQuery(sql);
+
+                foreach (DataRow row in ds.Tables["TABLE"].Rows)
+                {
+                    int stationID = Convert.ToInt32(row["TotalRiders"]);  // all rows have same station ID:
+                    string stationName = Convert.ToString(row["Name"]);
+                    stations.Add(new CTAStation(stationID, stationName));
+                }
+
+            }
+      catch (Exception ex)
+      {
+        string msg = string.Format("Error in Business.GetTopStations: '{0}'", ex.Message);
+        throw new ApplicationException(msg);
+      }
+
+      return stations;
+    }
+
+
+        //
+        //Get Number of Stations
+        //
+        public int getNumberOfStations()
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+            //query for Number of Stations
+            string sql = string.Format(@"SELECT Count(*) From Stations");
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            int numStations = Convert.ToInt32(result);
+
+            return numStations;
+        }
+
+        //
+        //Get Total Ridership
+        //
+        public long GetTotalRidership(int stationID)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+            //query for TotalRidership
+            string sql = string.Format(@"SELECT SUM(DailyTotal) AS TotalRidership FROM Riderships
+                                        INNER JOIN Stations
+                                        ON StationS.StationID = Riderships.StationID
+                                        WHERE Stations.StationID = '{0}'
+                                        ", stationID);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long totalRidership = Convert.ToInt64(result);
+
+            return totalRidership;
+        }
+
+        //
+        //Average Ridership at particular station
+        //
+        public long getAvgRidership(int stationID)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+            //query for AvgRidership
+            string sql = string.Format(@"SELECT AVG(DailyTotal) AS AVGRidership FROM Riderships
+                                        INNER JOIN Stations
+                                        ON StationS.StationID = Riderships.StationID
+                                        WHERE Stations.StationID = '{0}'
+                                        ", stationID);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long avgRidership = Convert.ToInt64(result);
+
+            return avgRidership;
+
+        }
+
+        //
+        // '%' Average at particular station
+        //
+        public long getTotalRidershipAcrossAllStations()
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query for %Ridership
+            string sql = string.Format(@"SELECT SUM(Convert(bigInt, DailyTotal)) AS AllRidership FROM Riderships
+                                        INNER JOIN Stations
+                                        ON StationS.StationID = Riderships.StationID");
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long allStationTotalRidership = Convert.ToInt64(result);
+
+            return allStationTotalRidership;
+
+        }
+
+        //
+        // Ridership for weekdays
+        //
+        public long getRidershipOnWeekDays(int stationID)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query to get Ridership for weekdays, saturday, sun/holiday
+            string sql = string.Format(@"SELECT Sum(DailyTotal) from Riderships
+                                          Inner Join Stations
+                                          ON stations.StationID = Riderships.StationID
+                                          Where Stations.StationID = '{0}' AND 
+                                          Riderships.TypeOfDay = 'W'", stationID);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long ridership = Convert.ToInt64(result);
+
+            return ridership;
+
+        }
+
+        //
+        // Ridership for weekdays
+        //
+        public long getRidershipOnSatuday(int stationID)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query to get Ridership for weekdays, saturday, sun/holiday
+            string sql = string.Format(@"SELECT Sum(DailyTotal) from Riderships
+                                          Inner Join Stations
+                                          ON stations.StationID = Riderships.StationID
+                                          Where Stations.StationID = '{0}' AND 
+                                          Riderships.TypeOfDay = 'A'", stationID);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long ridership = Convert.ToInt64(result);
+
+            return ridership;
+
+        }
+
+        //
+        // Ridership for weekdays
+        //
+        public long getRidershipOnSundayHoliday(int stationID)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query to get Ridership for weekdays, saturday, sun/holiday
+            string sql = string.Format(@"SELECT Sum(DailyTotal) from Riderships
+                                          Inner Join Stations
+                                          ON stations.StationID = Riderships.StationID
+                                          Where Stations.StationID = '{0}' AND 
+                                          Riderships.TypeOfDay = 'U'", stationID);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            long ridership = Convert.ToInt64(result);
+
+            return ridership;
+
+        }
+
+        //
+        //Is this stop Handicap Acceisible?
+        //
+        public Boolean handicapAccessible(string stopName)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query for Handicap Accessible
+            string sql = string.Format(@"Select ADA from Stops
+                                        Inner Join Stations
+                                        ON Stations.StationID = Stops.StationID
+                                        WHERE Stops.Name = '{0}'", stopName);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            bool value = Convert.ToBoolean(result);
+
+            return value;
+        }
+
+        //
+        //Direction of Travel
+        //
+        public char getDirection(string stopName)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query for Direction of travel
+            string sql = string.Format(@"Select Stops.Direction from Stops
+                                          Inner Join Stations
+                                          ON Stations.StationID = Stops.StationID
+                                          Where Stops.Name = '{0}'", stopName);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            char direction = Convert.ToChar(result);
+
+            return direction;
+        }
+
+        //
+        //Location Latitude
+        //
+        public string getLatitude(string stopName)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query for Location
+            string sql = string.Format(@"Select Stops.Latitude from Stops
+                                          Inner Join Stations
+                                          ON Stations.StationID = Stops.StationID
+                                          Where Stops.Name = '{0}'", stopName);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            string latitude = Convert.ToString(result);
+            return latitude;
+        }
+
+        //
+        //Location Longitude
+        //
+        public string getLongitude(string stopName)
+        {
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query for Location
+            string sql = string.Format(@"Select Stops.Longitude from Stops
+                                          Inner Join Stations
+                                          ON Stations.StationID = Stops.StationID
+                                          Where Stops.Name = '{0}'", stopName);
+
+            object result = dataTier.ExecuteScalarQuery(sql);
+            string longitude = Convert.ToString(result);
+            return longitude;
+        }
+
+        //
+        //Lines list
+        //
+        public List<String> getLines(string stopName)
+        {
+            List<String> lines = new List<string>();
+            DataAccessTier.Data dataTier = new DataAccessTier.Data(_DBFile);
+
+            //query to get lines...
+            string sql = string.Format(@"Select Color from Lines
+                                        Inner Join StopDetails
+                                        On StopDetails.LineID = Lines.LineID
+                                        Inner Join Stops
+                                        On Stops.StopID = StopDetails.StopID
+                                        Where Stops.Name = '{0}'", stopName);
+
+            DataSet ds = dataTier.ExecuteNonScalarQuery(sql);
+            foreach (DataRow row in ds.Tables["TABLE"].Rows)
+            {
+                string ld = Convert.ToString(row["Color"]);
+                lines.Add(ld);
+            }
+
+            return lines;
+        }
+    }//class
+}//namespace
---
+++ CTA/Form1.cs
@@ -0,0 +1,314 @@
+﻿//
+// N-tier C# and SQL program to analyze CTA Ridership data.
+//
+// <<SYED SHARIQ UR RAHMAN>>
+// U. of Illinois, Chicago
+// CS341, Fall2017
+// Project #08
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data.SqlClient;
+
+namespace CTA
+{
+
+  public partial class Form1 : Form
+  {
+    private string BuildConnectionString()
+    {
+      string version = "MSSQLLocalDB";
+      string filename = this.txtDatabaseFilename.Text;
+
+      string connectionInfo = String.Format(@"Data Source=(LocalDB)\{0};AttachDbFilename={1};Integrated Security=True;", version, filename);
+
+      return connectionInfo;
+    }
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      //
+      // setup GUI:
+      //
+      this.lstStations.Items.Add("");
+      this.lstStations.Items.Add("[ Use File>>Load to display L stations... ]");
+      this.lstStations.Items.Add("");
+
+      this.lstStations.ClearSelected();
+
+      toolStripStatusLabel1.Text = string.Format("Number of stations:  0");
+
+                string filename = this.txtDatabaseFilename.Text;
+                BusinessTier.Business bizTier;
+                bizTier = new BusinessTier.Business(filename);
+                bizTier.TestConnection();
+
+    }
+
+
+    //
+    // File>>Exit:
+    //
+    private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+
+    //
+    // File>>Load Stations:
+    //
+    private void toolStripMenuItem2_Click(object sender, EventArgs e)
+    {
+      //
+      // clear the UI of any current results:
+      //
+      ClearStationUI(true /*clear stations*/);
+
+        string filename = this.txtDatabaseFilename.Text;
+
+        var bizTier = new BusinessTier.Business(filename);
+
+        var stations = bizTier.GetStations();
+
+        foreach (var s in stations)
+        {
+            this.lstStations.Items.Add(s.Name);
+        }
+        int numStations = bizTier.getNumberOfStations();
+        toolStripStatusLabel1.Text = string.Format("Number of stations:  {0:#,##0}", numStations);
+      }
+
+
+    //
+    // User has clicked on a station for more info:
+    //
+    private void lstStations_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // sometimes this event fires, but nothing is selected...
+        if (this.lstStations.SelectedIndex < 0)   // so return now in this case:
+           return; 
+      
+        //
+        // clear GUI in case this fails:
+        //
+        ClearStationUI();
+
+        //
+        // now display info about selected station:
+        //
+        string stationName = this.lstStations.Text;
+        stationName = stationName.Replace("'", "''");
+
+        string filename = this.txtDatabaseFilename.Text;
+        var bizTier = new BusinessTier.Business(filename);
+        var stations = bizTier.GetStations();
+        int IDStation = stations[this.lstStations.SelectedIndex].ID;
+
+        //
+        //Display Station ID
+        //
+        string strStationID = Convert.ToString(IDStation); 
+        this.txtStationID.Text = strStationID;
+
+        //
+        //GET TOTAL RIDERSHIP...
+        //
+        long totalRidership = bizTier.GetTotalRidership(IDStation);
+        this.txtTotalRidership.Text = string.Format("{0:#,##0}", totalRidership);
+
+        //
+        //GET AVERAGE RIDERSHIP AT THAT PARTICULAR STATION
+        //
+        long avgridership = bizTier.getAvgRidership(IDStation);
+        this.txtAvgDailyRidership.Text = string.Format("{0:#,##0}/day", avgridership);
+
+        //
+        //GET %RIDERSHIP
+        //
+        long totalRidershipAllStations = bizTier.getTotalRidershipAcrossAllStations();
+        double perRidership = ((double)(totalRidership) / totalRidershipAllStations) * 100;
+        this.txtPercentRidership.Text = string.Format("{0:0.00}%", perRidership);
+
+        //
+        //Ridership for WeekDays
+        //
+        long weekDays = bizTier.getRidershipOnWeekDays(IDStation);
+        this.txtWeekdayRidership.Text = string.Format("{0:#,##0}", weekDays);
+
+        //
+        //Ridership for Satuday
+        //
+        long saturday = bizTier.getRidershipOnSatuday(IDStation);
+        this.txtSaturdayRidership.Text = string.Format("{0:#,##0}", saturday);
+
+        //
+        //Ridership for Sun/Holiday
+        //
+        long sunHoliday = bizTier.getRidershipOnSundayHoliday(IDStation);
+        this.txtSundayHolidayRidership.Text = string.Format("{0:#,##0}", sunHoliday);
+
+        //
+        //GET THE STATIONS USING BUSINESS TIER
+        //
+        var stops = bizTier.GetStops(IDStation);
+        foreach (var s in stops)
+        {
+            this.lstStops.Items.Add(s.Name);
+        }
+
+    }
+
+    private void ClearStationUI(bool clearStatations = false)
+    {
+      ClearStopUI();
+
+      this.txtTotalRidership.Clear();
+      this.txtTotalRidership.Refresh();
+
+      this.txtAvgDailyRidership.Clear();
+      this.txtAvgDailyRidership.Refresh();
+
+      this.txtPercentRidership.Clear();
+      this.txtPercentRidership.Refresh();
+
+      this.txtStationID.Clear();
+      this.txtStationID.Refresh();
+
+      this.txtWeekdayRidership.Clear();
+      this.txtWeekdayRidership.Refresh();
+      this.txtSaturdayRidership.Clear();
+      this.txtSaturdayRidership.Refresh();
+      this.txtSundayHolidayRidership.Clear();
+      this.txtSundayHolidayRidership.Refresh();
+
+      this.lstStops.Items.Clear();
+      this.lstStops.Refresh();
+
+      if (clearStatations)
+      {
+        this.lstStations.Items.Clear();
+        this.lstStations.Refresh();
+      }
+    }
+
+
+    //
+    // user has clicked on a stop for more info:
+    //
+    private void lstStops_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      // sometimes this event fires, but nothing is selected...
+      if (this.lstStops.SelectedIndex < 0)   // so return now in this case:
+        return; 
+
+      //
+      // clear GUI in case this fails:
+      //
+      ClearStopUI();
+
+      //
+      // now display info about this stop:
+      //
+      string stopName = this.lstStops.Text;
+      stopName = stopName.Replace("'", "''");
+
+        string filename = this.txtDatabaseFilename.Text;
+        var bizTier = new BusinessTier.Business(filename);
+        
+        //
+        //Handicap Accessible
+        //
+        bool value = bizTier.handicapAccessible(stopName);
+        string strValue = Convert.ToString(value);
+        string Accessible;
+        if (strValue == "True")
+           Accessible = "Yes";
+        else
+           Accessible = "No";
+        this.txtAccessible.Text = Accessible;
+
+        //
+        //Direction of Travel
+        //
+        char direction = bizTier.getDirection(stopName);
+        this.txtDirection.Text = Convert.ToString(direction);
+
+        //
+        //Location
+        //
+        string latitude = bizTier.getLatitude(stopName);
+        string longitude = bizTier.getLongitude(stopName);
+        this.txtLocation.Text = "(" + latitude + ", " + longitude + ")";
+
+        //
+        //Lines
+        //
+        List<String> lines = new List<string>();
+        lines = bizTier.getLines(stopName);
+
+        foreach(var line in lines)
+        {
+            this.lstLines.Items.Add(line);
+        }
+
+    }
+
+    private void ClearStopUI()
+    {
+      this.txtAccessible.Clear();
+      this.txtAccessible.Refresh();
+
+      this.txtDirection.Clear();
+      this.txtDirection.Refresh();
+
+      this.txtLocation.Clear();
+      this.txtLocation.Refresh();
+
+      this.lstLines.Items.Clear();
+      this.lstLines.Refresh();
+    }
+
+
+    //
+    // Top-10 stations in terms of ridership:
+    //
+    private void top10StationsByRidershipToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        //
+        // clear the UI of any current results:
+        //
+        ClearStationUI(true /*clear stations*/);
+
+        //
+        // now load top-10 stations:
+        //
+        //TOP 10 Stations using business tier
+        string filename = this.txtDatabaseFilename.Text;
+
+         var bizTier = new BusinessTier.Business(filename);
+
+         var stations = bizTier.GetTopStations(10);
+         int numStations = 0;
+         foreach (var s in stations)
+         {
+            this.lstStations.Items.Add(s.Name);
+            numStations++;
+         }
+        toolStripStatusLabel1.Text = string.Format("Number of stations:  {0:#,##0}", numStations);
+    }
+  }//class
+}//namespace
